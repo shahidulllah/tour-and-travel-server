ef321f1f293b024d79b78261c59a8a7bacfccc88
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-16 22:55:59 +0600

    server is working

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import app from "./app";
 import config from "./app/config";
+import mongoose from "mongoose";
+import {Server} from 'http';
+
+let server: Server;
+async function main() {
+  try{
+    await mongoose.connect(config.database_url as string);
+
+    server = app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`);
+      });
+
+  }catch(err){
+    console.log(err);
+  }
 
 
+}
 
-app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`);
-  });
+main();
+
+
