2f447587dec97b193e786df33cbe04618c16c239
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-16 22:03:08 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
