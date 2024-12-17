5894a32c819ff6d5c25b9c42f5c4ffb86d64218d
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-17 18:35:11 +0600

    Working on prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,12 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
+module.exports = [
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+];
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,35 @@
-import app from "./app";
-import config from "./app/config";
-import mongoose from "mongoose";
-import {Server} from 'http';
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
+import { Server } from 'http';
 
 let server: Server;
 async function main() {
-  try{
+  try {
     await mongoose.connect(config.database_url as string);
 
     server = app.listen(config.port, () => {
-        console.log(`Example app listening on port ${config.port}`);
-      });
-
-  }catch(err){
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
     console.log(err);
   }
-
-
 }
 
 main();
 
-process.on('unhandledRejection', ()=> {
-    console.log(`😈 UnhandledRejection is detected, sutting down ...`);
-  
-    if(server){
-      server.close(() => {
-        process.exit(1)
-      })
-    }
-    process.exit(1)
-  })
-  
-  process.on('uncaughtException', () => {
-    console.log(`😈 uncaughtException is detected , shutting down ...`);
-    process.exit(1);
-  });
+process.on('unhandledRejection', () => {
+  console.log(`😈 UnhandledRejection is detected, sutting down ...`);
 
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
 
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
