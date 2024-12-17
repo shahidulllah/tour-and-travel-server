56e1580917f0b080a38d494f32fb992d34a0ae3f
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-17 20:25:01 +0600

    create send Response

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,13 +1,18 @@
 import { Response } from 'express';
 
-
 type TResponse<T> = {
-    statusCode: number;
-    success: boolean;
-    message?: string;
-    data: T;
-}
+  statusCode: number;
+  success: boolean;
+  message?: string;
+  data: T;
+};
 
 const sendRenponse = <T>(res: Response, data: TResponse<T>) => {
-    
-}
+  res.status(data?.statusCode).json({
+    success: data.success,
+    message: data.message,
+    data: data.data,
+  });
+};
+
+export default sendRenponse;
